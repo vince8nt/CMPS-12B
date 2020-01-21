b2be784197e66d08f4e54e24b16eb6ab85df7f6f
Vincent Titterton <57516281+vince8nt@users.noreply.github.com>
2020-01-20 21:37:51 -0800

    Update FileReverse.c

--- lab3/FileReverse.c
+++ lab3/FileReverse.c
@@ -1,21 +1,64 @@
 /*
- * FileReverse.cpp
- *
- *  Created on: Jan 27, 2019
- *      Author: vincent
- */
+* Vincent Titterton
+* vtittert
+* 12M
+* 1/27/2019
+* Takes a file as input and creates a file with each
+* word from the input reversed and on a new line
+*/
 
-#include "FileReverse.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
 
-namespace FileReverse {
-
-FileReverse::FileReverse() {
-	// TODO Auto-generated constructor stub
-
+void stringReverse(char* s)
+{
+    int i = 0;
+    int j = strlen(s) - 1;
+    while (j - i > 0)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
 }
 
-FileReverse::~FileReverse() {
-	// TODO Auto-generated destructor stub
-}
 
-} /* namespace FileReverse */
+int main(int argc, char* argv[])
+{
+	FILE* in; /* file handle for input */
+ 	FILE* out; /* file handle for output */
+ 	char word[256]; /* char array to store words from input file */
+ /* check command line for correct number of arguments */
+	if( argc != 3 )
+	{
+		printf("Usage: %s <input file> <output file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+ /* open input file for reading */
+	in = fopen(argv[1], "r");
+	if( in==NULL )
+	{
+		printf("Unable to read from file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+ /* open output file for writing */
+	out = fopen(argv[2], "w");
+	if( out==NULL )
+	{
+		printf("Unable to write to file %s\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+ /* read words from input file, print on separate lines to output file*/
+	while( fscanf(in, " %s", word) != EOF )
+	{
+        stringReverse(word);
+		fprintf(out, "%s\n", word);
+	}
+ /* close input and output files */
+	fclose(in);
+	fclose(out);
+	return(EXIT_SUCCESS);
+}
